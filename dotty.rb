0d03c30537a994e7fa5aa7a4f9e8edc1b18663a5
Adam Hall <adamhalldesigns@gmail.com>
2022-08-13 15:24:20 +0100

    v1.0.3

---
+++ dotty.rb
@@ -0,0 +1,24 @@
+require "language/go"
+
+class Dotty < Formula
+  desc "dotfiles manager"
+  homepage "https://github.com/codethread/dotty"
+  url "https://github.com/codethread/dotty/archive/refs/tags/v1.0.3.tar.gz"
+  sha256 "9edeb52e15f94212e5c240a1d14e484edb534bee"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    path = buildpath/"src/github.com/miguelmota/cointop"
+    path.install Dir["*"]
+    cd path do
+      system "go", "build", "-o", "#{bin}/dotty"
+    end
+  end
+
+  test do
+    system "true"
+  end
+end
