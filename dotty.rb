c41d445116feaae614d5d2183ddc978952649679
Adam Hall <adamhalldesigns@gmail.com>
2022-08-15 18:07:35 +0100

    bump

--- dotty.rb
+++ dotty.rb
@@ -3,8 +3,9 @@ require "language/go"
 class Dotty < Formula
   desc "dotfiles manager"
   homepage "https://github.com/codethread/dotty"
-  url "https://github.com/codethread/dotty/archive/refs/tags/v1.0.7.tar.gz"
-  sha256 "debc3e41ebaba0988a3a1f4d999e10e86dcf39c75be0049d8e642fa690ebd61a"
+  url "https://github.com/codethread/dotty/archive/refs/tags/v1.0.10.tar.gz"
+  sha256 "b5fe51065153c71688b1450e1df1fede6c8244bd513b0256441999659715f8aa"
+
   license "MIT"
 
   depends_on "go" => :build
